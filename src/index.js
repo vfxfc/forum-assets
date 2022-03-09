19e4ccee46482109b687444aa7482016c042ebfc
vfxfc <zxf@vfxfc.cn>
2022-03-09 19:13:14 +0800

    .

--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ $(document).ready(function(){
     
   });
   $("hr").remove();
-  $("body").children().last().text('validate failed');
+  $("body").children().last().html("Powered by <a href='https://www.freeflarum.com' target='_blank'>FreeFlarum</a>");
 });
