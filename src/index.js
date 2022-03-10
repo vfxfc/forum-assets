afe23786edf162b2e50150db585b55e1a0ff69cb
vfxfc <zxf@vfxfc.cn>
2022-03-10 23:40:28 +0800

    .

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
-  // alert('ddd');
-  $(".Hero-title").click(function(){
+  // $(".Hero-title").click(function(){
     
-  });
+  // });
   $("hr").remove();
   $("body").children().last().html("Powered by <a href='https://www.freeflarum.com' target='_blank'>FreeFlarum</a>");
 
+  $(".Afrux-NewsWidget-icon").remove();
+
+  
 });
